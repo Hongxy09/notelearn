0df3fb3f10f1adad88ae0846d1b34eb94cf9ef04
Hongxy09 <17853100535@163.com>
2021-02-14 17:02:18 +0800

    修改文件路径，进行分类

--- deeplearn_note/deeplearn.py
+++ deeplearn_note/deeplearn.py
@@ -46,15 +46,18 @@ def sigmoid(x):#s形函数
     return 1/(1+np.exp(-x))
 def relu(x):
     return np.maximum(0,x)
-def softmax(a):
+def softmax(x):
     '''概率函数，y[0]的概率...y[1]的概率...
     yk=exp(xk)/sum-exp(xi)[1<=i<=n]
     防止指数函数运算溢出，将分子分母分别在指数内加一个系统上限数或者输入函数中的最大数'''
-    c=np.max(a)
-    exp_a=np.exp(a-c)
-    sum_exp_a=np.sum(exp_a)
-    y=exp_a/sum_exp_a
-    return y
+    if x.ndim == 2:
+        x = x.T#求x的转置
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T 
+
+    x = x - np.max(x) # 溢出对策
+    return np.exp(x) / np.sum(np.exp(x))
 def sigmoid_grad(x):
     return (1.0 - sigmoid(x)) * sigmoid(x)
 def relu_grad(x):
@@ -348,3 +351,50 @@ class TwoLayerNet:
         grads['b1'] = np.sum(dz1, axis=0)
 
         return grads
+def done_mini_batch_in_TwoLayerNet():
+    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+    network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
+
+    iters_num = 10000  # 适当设定循环的次数
+    train_size = x_train.shape[0]
+    batch_size = 100
+    learning_rate = 0.1
+
+    train_loss_list = []
+    train_acc_list = []
+    test_acc_list = []
+    iter_per_epoch = max(train_size / batch_size, 1)
+    for i in range(iters_num):
+        batch_mask = np.random.choice(train_size, batch_size)
+        x_batch = x_train[batch_mask]
+        t_batch = t_train[batch_mask]
+        
+        # 计算梯度
+        #grad = network.numerical_gradient(x_batch, t_batch)
+        grad = network.gradient(x_batch, t_batch)
+        
+        # 更新参数
+        for key in ('W1', 'b1', 'W2', 'b2'):
+            network.params[key] = network.params[key] - learning_rate * grad[key]
+        #记录学习过程
+        loss = network.loss(x_batch, t_batch)
+        train_loss_list.append(loss)
+        
+        if i % iter_per_epoch == 0:#epoch是指程序循环几次minibatch才能把所有的训练数据看完
+            train_acc = network.accuracy(x_train, t_train)
+            test_acc = network.accuracy(x_test, t_test)
+            train_acc_list.append(train_acc)
+            test_acc_list.append(test_acc)
+            print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+
+    # 绘制图形
+    _markers = {'train': 'o', 'test': 's'}
+    x = np.arange(len(train_acc_list))
+    plt.plot(x, train_acc_list, label='train acc')
+    plt.plot(x, test_acc_list, label='test acc', linestyle='--')
+    plt.xlabel("epochs")
+    plt.ylabel("accuracy")
+    plt.ylim(0, 1.0)
+    plt.legend(loc='lower right')
+    plt.show()
+done_mini_batch_in_TwoLayerNet()
